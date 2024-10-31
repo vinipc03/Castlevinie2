168665981e12ec22741e696e511f0e094e9f92d6
Vinícius Costa <vini.pc03@gmail.com>
2024-10-31 18:31:08 -0300

    Player jump added

--- Castlevini2/Assets/Game Folder/Player/Script/PlayerController.cs
+++ Castlevini2/Assets/Game Folder/Player/Script/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 vel;
+
+    public Transform floorCollider;
+    public LayerMask floorLayer;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +21,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+
+        bool canJump = Physics2D.OverlapCircle(floorCollider.position, 0.2f, floorLayer);
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(0, 200));
+        }
+
+        Movement();
     }
 
     private void FixedUpdate()
     {
        rb.velocity = vel;
+    }
+
+    // MOVIMENTO ESQUERDA/DIREITA
+    private void Movement()
+    {
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
     }
 
 }
