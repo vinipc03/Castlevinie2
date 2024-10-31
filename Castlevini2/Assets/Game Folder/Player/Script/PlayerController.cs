e7fc3925add58336ae5fd3501370d24411eb000f
Vinícius Costa <vini.pc03@gmail.com>
2024-10-31 00:47:20 -0300

    Horizontal movement added

---
+++ Castlevini2/Assets/Game Folder/Player/Script/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+       rb.velocity = vel;
+    }
+
+}
