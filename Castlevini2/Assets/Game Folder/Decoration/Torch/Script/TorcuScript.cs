7d4ac55749de48fc0230c94f3fa2b2b81e4b9f89
Vinícius Costa <vini.pc03@gmail.com>
2024-12-07 18:35:49 -0300

    Torch loot system ok

---
+++ Castlevini2/Assets/Game Folder/Decoration/Torch/Script/TorcuScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorcuScript : MonoBehaviour
+{
+    public Transform torchPrefab;
+
+    [System.Serializable]
+    public class DropItem
+    {
+        public GameObject itemPrefab; // O prefab do item
+        public float dropChance;      // Chance de soltar o item
+    }
+
+    public DropItem[] possibleDrops; // Lista de itens possíveis
+
+    public void Break()
+    {
+        torchPrefab.GetComponent<Animator>().Play("Broke", -1);
+
+        // Espera o tempo da animação antes de destruir o objeto
+        float animationDuration = torchPrefab.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length;
+        Destroy(gameObject, animationDuration);
+        // Destroi o objeto
+        Destroy(gameObject);
+        
+        // Sorteia um item para soltar
+        float randomValue = Random.Range(0f, 100f);
+        float cumulativeChance = 0f;
+
+        foreach (DropItem drop in possibleDrops)
+        {
+            cumulativeChance += drop.dropChance;
+            if (randomValue <= cumulativeChance)
+            {
+                // Instancia o item no local do objeto
+                Instantiate(drop.itemPrefab, transform.position, Quaternion.identity);
+                return; // Apenas um item será solto
+            }
+        }
+    }
+
+    // Exemplo de detecção de ataque
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerAttack"))
+        {
+            
+            Break();
+        }
+    }
+}
--- Castlevini2/Assets/Game Folder/PowerUps/PowerUpScript.cs
+++ Castlevini2/Assets/Game Folder/PowerUps/PowerUpScript.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PowerUpScript : MonoBehaviour
 {
     private Rigidbody2D itemRb;
-    public float dropForce = 5;
+    public float dropForce = 2;
     public int hpPot;
     public int mpPot;
     public int hpHeal;
@@ -17,10 +17,18 @@ public class PowerUpScript : MonoBehaviour
         itemRb = GetComponent<Rigidbody2D>();
         itemRb.AddForce(Vector2.up * dropForce, ForceMode2D.Impulse);
 
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Floor"))
+        {
+            // Para o movimento após cair no chão
+            itemRb.velocity = Vector2.zero;
+            itemRb.isKinematic = true; // Desativa física
+        }
+
         if (collision.CompareTag("Player"))
         {
             // POÇÃO DE VIDA
@@ -46,5 +54,15 @@ public class PowerUpScript : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.collider.CompareTag("Floor"))
+    //    {
+    //        // Para o movimento após cair no chão
+    //        itemRb.velocity = Vector2.zero;
+    //        itemRb.isKinematic = true; // Desativa física
+    //    }
+    //}
 
 }
