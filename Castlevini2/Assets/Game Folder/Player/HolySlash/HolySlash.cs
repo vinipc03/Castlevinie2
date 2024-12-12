ae9208d001ba3acf212acc452717073e2b2c10a0
Vinícius Costa <vini.pc03@gmail.com>
2024-12-12 18:34:42 -0300

    Holy Slash update

---
+++ Castlevini2/Assets/Game Folder/Player/HolySlash/HolySlash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolySlash : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Destroy(this.gameObject, 0.5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Destroy(impactEffect, 1f);
+        Instantiate(impactEffect, transform.position, transform.rotation);
+
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Character>().PlayerDamage(3);
+        }
+
+    }
+}
---
+++ Castlevini2/Assets/Game Folder/Player/HolySlash/HolySlashController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolySlashController : MonoBehaviour
+{
+    public Transform attackCollider;
+    public GameObject holySlashPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void HolySlash()
+    {
+        // ATIRAR HOLY BOLT
+        Instantiate(holySlashPrefab, attackCollider.position, attackCollider.rotation);
+    }
+}
