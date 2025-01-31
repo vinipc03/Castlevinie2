de4bfaa6fcd19f51f0f83394303a3423f2fa19cf
Vinícius Costa <vini.pc03@gmail.com>
2025-01-31 16:01:46 -0300

    Iframes and level camera transition

--- Castlevini2/Assets/Game Folder/Character/Script/Character.cs
+++ Castlevini2/Assets/Game Folder/Character/Script/Character.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public int life;
-    public int maxLife;
+    private int maxLife;
     public int mana;
-    public int maxMana;
+    private int maxMana;
     public Transform skin;
 
     public LifeController lifeBar;
     public ManaController manaBar;
     public float kbForce;
     private PlayerController playerController;
+
+    [Header("iframes")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+    private void Awake()
+    {
+        //spriteRend = GetComponent<SpriteRenderer>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -53,6 +63,7 @@ public class Character : MonoBehaviour
             GetComponent<PlayerController>().audioSource.PlayOneShot(GetComponent<PlayerController>().takeDamageSound, 0.4f);
             lifeBar.SetLife(life);
             GetComponent<PlayerController>().onAttack = false;
+            StartCoroutine(Invunerability());
         }
     }
 
@@ -100,5 +111,18 @@ public class Character : MonoBehaviour
         {
             mana = maxMana;
         }
+    }
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            skin.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes*2));
+            skin.GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
